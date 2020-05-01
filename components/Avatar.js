53cc51f3d138135f7396e49781038bb88fc83029
Jakub Nagiet <jakub.nagiet@gmail.com>
2020-05-01 09:48:44 +0200

    asd

---
+++ components/Avatar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+class Avatar extends React.Component {
+  state = {
+    photo: "https://cl.ly/55da82beb939/download/avatar-default.jpg",
+  };
+
+  componentDidMount() {
+    fetch("https://uifaces.co/api")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+
+        this.setState({
+          photo: response.photo,
+        });
+      });
+  }
+
+  render() {
+    return <Image source={{ uri: this.state.photo }} />;
+  }
+}
+
+export default Avatar;
+
+const Image = styled.Image`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  margin-left: 20px;
+`;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,6 +14,7 @@ import Logo from "../components/Logo";
 import Course from "../components/Course";
 import Menu from "../components/Menu";
 import { connect } from "react-redux";
+import Avatar from "../components/Avatar";
 
 function mapStateToPros(state) {
   return {
@@ -92,7 +93,7 @@ class HomeScreen extends React.Component {
                   onPress={this.props.openMenu}
                   style={{ position: "absolute", top: 0, left: 0 }}
                 >
-                  <Avatar source={require("../assets/avatar.jpg")} />
+                  <Avatar />
                 </TouchableOpacity>
                 <Title>Welcome back,</Title>
                 <Name>Meng</Name>
@@ -172,14 +173,6 @@ const Subtitle = styled.Text`
   margin-left: 20px;
   margin-top: 10px;
   text-transform: uppercase;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
 `;
 
 const Container = styled.View`
